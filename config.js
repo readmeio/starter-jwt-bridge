95468d0186ab2660dffe407410cd713f79796d93
Marc <mjcuva@gmail.com>
2016-08-10 11:46:58 -0700

    Fix url

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ exports.loginCallback = function(body){
 
 // Readme config -- automicatically configured for your project
 exports.readmeConfig = {
-  redirect_uri: "http://localhost:3002/p/readme/oauth/callback",
+  redirect_uri: "http://localhost:3002/p/README_PROJECT/oauth/callback",
   readme_url: README_URL,
   jwt_secret: JWT_SECRET
 };
