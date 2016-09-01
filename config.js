bf581c59a8a06bc87475d7d332ad9f6f41735939
Marc <mjcuva@gmail.com>
2016-09-01 14:53:15 -0700

    Fix up config file

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 exports.config = {
-  clientID: "",
-  clientSecret: "",
-  authorizationPath: "",
-  tokenPath: "",
-  profilePath: "",
-  scope: "",
+  clientID: '',
+  clientSecret: '',
+  authorizationPath: '',
+  tokenPath: '',
+  profilePath: '',
+  scope: '',
 };
 
 // At this point the user has been authenticated and body contains
 // the info your server returned about them.
 // Return the user info you want to pass to readme
-exports.loginCallback = function(body){
+exports.loginCallback = function(body) {
   // See readme.readme.io/jwt for more specific info about the format
   return {
-  	'email': body.email,
-    'name': body.name,
-    'keys': {
-			api_key: body.api_key,
-      name: body.project_name
-    }
+    email: body.email,
+    name: body.name,
+    keys: {
+      api_key: body.api_key,
+      name: body.project_name,
+    },
   };
 };
 
 // Readme config -- automicatically configured for your project
 exports.readmeConfig = {
-  redirect_uri: "https://oauth.readme.io/p/README_PROJECT/oauth/callback",
-  // readme_url: README_URL,
-  // jwt_secret: JWT_SECRET
+  redirect_uri: 'https://oauth.readme.io/p/README_PROJECT/oauth/callback',
+  readme_url: README_URL,
+  jwt_secret: JWT_SECRET,
 };
